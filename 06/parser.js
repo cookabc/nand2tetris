2da7b9d3a6afb42a97cb6a07f4371414743424cd
chuangcius <chuangcius@gmail.com>
2019-12-23 09:02:14 +0800

    Add Parser for project 06

---
+++ 06/parser.js
@@ -0,0 +1,60 @@
+module.exports = class Parser {
+  constructor(inputFile) {
+    const commands = fs.readFileSync(inputFile, 'utf-8')
+    this.commands = commands.split('\n')
+    this.currentCommand = null
+    this.currentCommandType = null
+  }
+
+  hasMoreCommands() {
+    return this.commands.length > 0
+  }
+
+  advance() {
+    const command = this.commands.shift()
+    this.currentCommand = command.replace(/\s|(\/\/).+/g, '')
+  }
+
+  commandType() {
+    if (!this.currentCommand) {
+      this.currentCommandType = null
+    } else if (this.currentCommand.startsWith('@')) {
+      this.currentCommandType = 'A'
+    } else if (this.currentCommand.startsWith('(')) {
+      this.currentCommandType = 'L'
+    } else {
+      this.currentCommandType = 'C'
+    }
+    return this.currentCommandType
+  }
+
+  symbol() {
+    if (this.currentCommandType === 'A') {
+      return this.currentCommand.slice(1)
+    } else if (this.currentCommandType === 'L') {
+      return this.currentCommand.replace(/^\((.+)\)$/, '$1')
+    }
+  }
+
+  comp() {
+    if (this.currentCommandType === 'C' && this.currentCommand.contains("=")) {
+      return currentCommand.split("=")[0]
+    }
+  }
+
+  dest() {
+    if (this.currentCommandType === 'C') {
+      if (this.currentCommand.contains("=")) {
+        return this.currentCommand.split("=")[1]
+      } else if (this.currentCommand.contains(";")) {
+        return this.currentCommand.split(";")[0]
+      }
+    }
+  }
+
+  jump() {
+    if (this.currentCommandType === 'C' && this.currentCommand.contains(';')) {
+      return this.currentCommand.split(";")[1]
+    }
+  }
+}
