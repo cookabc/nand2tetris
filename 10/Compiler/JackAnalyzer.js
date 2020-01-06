95a79127292f35becea78e840dbbbbe17a613512
chuangcius <chuangcius@gmail.com>
2020-01-06 22:43:45 +0800

    start compile

--- 10/Compiler/JackAnalyzer.js
+++ 10/Compiler/JackAnalyzer.js
@@ -5,3 +5,5 @@ const CompilationEngine = require("./CompilationEngine");
 const inputFile = process.argv[2]
 const tokenizer = new JackTokenizer(inputFile);
 const compilationEngine = new CompilationEngine(tokenizer);
+
+compilationEngine.compileClass()
