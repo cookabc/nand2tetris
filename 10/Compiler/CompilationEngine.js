a38307f9a9c972963d3da024b93d49509de07fa5
chuangcius <chuangcius@gmail.com>
2020-01-07 17:18:00 +0800

    Add compileClass implementation for Compiler

--- 10/Compiler/CompilationEngine.js
+++ 10/Compiler/CompilationEngine.js
@@ -1,9 +1,92 @@
 module.exports = class CompilationEngine {
   constructor(tokenizer) {
     this.tokenizer = tokenizer
+    this.output = []
+    this.spacing = ""
   }
 
-  compileClass() { }
+  writeTag(word, type) {
+    this.output.push(this.spacing + "<" + type + "> " + word + " </" + type + ">")
+  }
+
+  increaseSpacing() {
+    this.spacing += "\t"
+  }
+
+  decreaseSpacing() {
+    this.spacing = this.spacing.substring(1)
+  }
+
+  checkSymbol(s) {
+    if (s === "<") { s = "&lt;" }
+    else if (s === ">") { s = "&gt;" }
+    else if (s === "&") { s = "&amp;" }
+
+    if (this.tokenizer.symbol() === s) {
+      this.writeTag(s, "symbol");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  compileClass() {
+    this.tokenizer.advance()
+    if (this.tokenizer.keyword() == "CLASS") {
+      // write class
+      this.output.push(this.spacing + "<class>")
+      this.increaseSpacing()
+
+      this.writeTag(this.tokenizer.token(), "keyword")
+
+      // write class name
+      this.tokenizer.advance()
+      if (this.tokenizer.tokenType() == "IDENTIFIER") {
+        this.writeTag(this.tokenizer.identifier(), "identifier")
+      } else {
+        console.warn("illegal class name identifier")
+        return
+      }
+
+      // write {
+      this.tokenizer.advance()
+      if (!this.checkSymbol("{")) {
+        console.warn("no openning { for class")
+        return
+      }
+
+      // parse potential classVarDec
+      this.tokenizer.advance()
+      while (["STATIC", "FIELD"].includes(this.tokenizer.keyword())) {
+        this.compileClassVarDec()
+        this.tokenizer.advance()
+      }
+
+      // parse potential subroutineDec
+      while (["CONSTRUCTOR", "FUNCTION", "METHOD"].includes(this.tokenizer.keyword())) {
+        this.compileSubRoutine()
+        this.tokenizer.advance()
+      }
+
+      // write }
+      if (!this.checkSymbol("}")) {
+        console.warn("no closing } for class")
+        return
+      }
+
+      if (this.tokenizer.hasMoreTokens()) {
+        console.warn("addtional tokens after closing }")
+      }
+
+      // write close tag of class
+      this.decreaseSpacing()
+      this.output.push(this.spacing + "</class>")
+      this.output.forEach(o => console.log(o))
+    } else {
+      console.warn("does not start with class")
+      return
+    }
+  }
   compileClassVarDec() { }
   compileSubroutine() { }
   compileParameterList() { }
--- 10/Compiler/JackTokenizer.js
+++ 10/Compiler/JackTokenizer.js
@@ -3,23 +3,23 @@ const fs = require("fs")
 const symbolPattern = "(.*)(\\{|\\}|\\(|\\)|\\[|\\]|\\.|\\,|;|\\+|-|\\*|/|&|\\||<|>|=|~)(.*)"
 const commentPattern = "(^//.*)|(^/\\*.*)|^\\*.*"
 const keywordPattern = "class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return"
-const identifierPattern = "^[^\\d\\W]\\w*\\Z"
+const identifierPattern = "^[^\d\W]\w*"
 const intPattern = "\\d+"
 const stringPattern = "^\"[^\"]+\"$"
 
-const keywordList = ["class", "method", "function", "constructor", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "false", "null", "this"]
+const keywordList = ["class", "constructor", "function", "method", "field", "static", "var", "int", "char", "boolean", "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return"]
 
 module.exports = class JackTokenizer {
   constructor(inputFile) {
+    this.tokens = []
     const inputStream = fs.readFileSync(inputFile, "utf-8")
     const tokenLines = inputStream.split("\n")
     tokenLines.forEach(i => {
-      const tokenLine = i.replace(/\s|(\/\/).+/g, " ")
+      const tokenLine = i.replace(commentPattern, " ")
       const tokens = tokenLine.trim().split(" ")
-      this.tokens = [...this.tokens, ...tokens]
+      tokens.forEach(t => { if (t) this.tokens.push(t) })
     })
     this.currentToken = null
-    this.currentTokenType = null
   }
 
   hasMoreTokens() {
@@ -32,29 +32,34 @@ module.exports = class JackTokenizer {
     }
   }
 
-  tokenType() {
-    if (this.currentToken.match(keywordPattern)) {
-      this.currentTokenType = "KEYWORD"
-    } else if (this.currentToken.match(symbolPattern)) {
-      this.currentTokenType = "SYMBOL"
-    } else if (this.currentToken.match(identifierPattern)) {
-      this.currentTokenType = "IDENTIFIER"
-    } else if (this.currentToken.match(intPattern)) {
-      this.currentTokenType = "INT_CONST"
-    } else if (this.currentToken.match(stringPattern)) {
-      this.currentTokenType = "STRING_CONST"
-    }
-    return this.currentTokenType
+  token() {
+    return this.currentToken
   }
 
-  keyWord() {
+  tokenType() {
+    let tokenType
+    if (this.currentToken.match(keywordPattern)) {
+      tokenType = "KEYWORD"
+    } else if (this.currentToken.match(symbolPattern)) {
+      tokenType = "SYMBOL"
+    } else if (this.currentToken.match(identifierPattern)) {
+      tokenType = "IDENTIFIER"
+    } else if (this.currentToken.match(intPattern)) {
+      tokenType = "INT_CONST"
+    } else if (this.currentToken.match(stringPattern)) {
+      tokenType = "STRING_CONST"
+    }
+    return tokenType
+  }
+
+  keyword() {
     if (this.tokenType() === "KEYWORD" && keywordList.includes(this.currentToken)) {
       return this.currentToken.toUpperCase()
     }
   }
 
   symbol() {
-    if (this.currentTokenType === "SYMBOL") {
+    if (this.tokenType() === "SYMBOL") {
       if (this.currentToken === "<") { return "&lt;" }
       else if (this.currentToken === ">") { return "&gt;" }
       else if (this.currentToken === "&") { return "&amp;" }
