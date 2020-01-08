5182486426bf11c6b953183fa86c79c5a8556861
chuangcius <chuangcius@gmail.com>
2020-01-08 13:50:25 +0800

    Add output file

--- 10/Compiler/JackAnalyzer.js
+++ 10/Compiler/JackAnalyzer.js
@@ -1,9 +1,33 @@
-const JackTokenizer = require("./JackTokenizer");
-const CompilationEngine = require("./CompilationEngine");
+const fs = require("fs")
 
+const JackTokenizer = require("./JackTokenizer")
+const CompilationEngine = require("./CompilationEngine")
 
-const inputFile = process.argv[2]
-const tokenizer = new JackTokenizer(inputFile);
-const compilationEngine = new CompilationEngine(tokenizer);
+const fileName = process.argv[2]
+const isDirectory = fs.lstatSync(fileName).isDirectory()
 
-compilationEngine.compileClass()
+if (isDirectory) {
+  fs.readdir(fileName, (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+      processFileData(`${fileName}/${file}`)
+    })
+  })
+} else {
+  processFileData(fileName)
+}
+
+function processFileData(inputFile) {
+  if (inputFile.endsWith(".jack")) {
+    const tokenizer = new JackTokenizer(inputFile)
+    const compilationEngine = new CompilationEngine(tokenizer)
+    compilationEngine.compileClass()
+    writeOutFile(inputFile.split(".jack").join("_new.xml"), compilationEngine.output.join("\n"))
+  }
+}
+
+function writeOutFile(name, data) {
+  fs.writeFile(name, data, (err) => {
+    if (err) throw err
+  })
+}
